ef33b6cabe4af7694f91ca5d625da73738d43e2e
Matteo Zaccagnino <matteozaccagnino@mail.com>
2016-08-23 18:38:30 +0200

    bug fixes

--- lyricsifier/core/crawler.py
+++ lyricsifier/core/crawler.py
@@ -9,12 +9,12 @@ class LyricsComCrawler:
     def __init__(self):
         self.baseUrl = 'http://www.lyrics.com'
         self.lyricsUrlPattern = '{:s}/{:s}-lyrics-{:s}.html'
-        self.normalizingRegex = re.compile('[^a-zA-Z0-9]')
+        self.normalizingRegex = re.compile('[^a-zA-Z0-9\s-]')
         self.logger = logging.getLogger('lyricsifier.crawler.LyricsComCrawler')
 
     def _formatLyricsUrl(self, artist, title):
-        a = re.sub(' +', '-', self.normalizingRegex.sub(' ', artist)).lower()
-        t = re.sub(' +', '-', self.normalizingRegex.sub(' ', title)).lower()
+        a = re.sub(' +', '-', self.normalizingRegex.sub('', artist)).lower()
+        t = re.sub(' +', '-', self.normalizingRegex.sub('', title)).lower()
         return self.lyricsUrlPattern.format(self.baseUrl, t, a)
 
     def crawl(self, artist, title):
@@ -28,8 +28,8 @@ class LyricsComCrawler:
             soup = BeautifulSoup(html, 'html.parser')
             div = soup.find(id='lyrics')
             if div:
-                lyrics = div.get_text()
-                self.logger.debug('lyrics found\n{:s}'.format(lyrics))
+                lyrics = div.get_text().encode('utf8')
+                self.logger.debug('lyrics found\n{}'.format(lyrics))
                 return lyrics
             else:
                 self.logger.warning('cannot find lyrics inside of html')
@@ -39,18 +39,21 @@ class LyricsComCrawler:
             self.logger.warning('cannot find lyrics')
             return None
 
+    def stripLyricsAuthor(self, lyrics):
+        return re.sub('---\n?\s?[lL]yrics submitted by .+$', '', lyrics)
+
 
 class LyricsModeCrawler:
 
     def __init__(self):
         self.baseUrl = 'http://www.lyricsmode.com'
         self.lyricsUrlPattern = '{:s}/lyrics/{:1}/{:s}/{:s}.html'
-        self.normalizingRegex = re.compile('[^a-zA-Z0-9]')
+        self.normalizingRegex = re.compile('[^a-zA-Z0-9\s-]')
         self.logger = logging.getLogger('lyricsifier.crawler.LyricsModeCrawler')
 
     def _formatLyricsUrl(self, artist, title):
-        a = re.sub(' +', '-', self.normalizingRegex.sub(' ', artist)).lower()
-        t = re.sub(' +', '-', self.normalizingRegex.sub(' ', title)).lower()
+        a = re.sub(' +', '-', self.normalizingRegex.sub('', artist)).lower()
+        t = re.sub(' +', '-', self.normalizingRegex.sub('', title)).lower()
         return self.lyricsUrlPattern.format(self.baseUrl, a[0], a, t)
 
     def crawl(self, artist, title):
@@ -65,8 +68,8 @@ class LyricsModeCrawler:
                 soup = BeautifulSoup(html, 'html.parser')
                 p = soup.find(id='lyrics_text')
                 if p:
-                    lyrics = p.get_text()
-                    self.logger.debug('lyrics found\n{:s}'.format(lyrics))
+                    lyrics = p.get_text().encode('utf8')
+                    self.logger.debug('lyrics found\n{}'.format(lyrics))
                     return lyrics
                 else:
                     self.logger.warning('cannot find lyrics inside of html')

301dc219e12d78c5c7781ffc0ea559b704bc79df
Matteo Zaccagnino <matteozaccagnino@mail.com>
2016-08-23 18:38:58 +0200

    added crawl job

--- lyricsifier/core/crawler.py
+++ lyricsifier/core/crawler.py
@@ -1,4 +1,6 @@
+import csv
 import logging
+import os
 import re
 import urllib2
 from bs4 import BeautifulSoup
@@ -85,3 +87,55 @@ class LyricsModeCrawler:
                 return None
             else:
                 raise error
+
+    def stripLyricsAuthor(self, lyrics):
+        return lyrics
+
+
+class CrawlJob:
+
+    def __init__(self, tsv, outdir, crawlers=[LyricsComCrawler(), ]):
+        self.fin = tsv
+        self.fout = os.path.join(outdir, 'lyrics.txt')
+        self.crawlers = crawlers
+        self.outTsvFields = ['trackid', 'lyrics']
+        self.normalizingRegex = re.compile('[^a-zA-Z0-9]')
+        self.logger = logging.getLogger('lyricsifier.crawler.CrawlJob')
+        if not os.path.exists(outdir):
+            self.logger.warning('output directory does not exist - creating it...')
+            os.makedirs(outdir)
+
+    def _inline(self, lyrics):
+        l = self.normalizingRegex.sub(' ', lyrics)
+        return re.sub(' +', ' ', l).lower()
+
+    def run(self):
+        with open(self.fin, 'rb') as tsvin, open(self.fout, 'wb') as tsvout:
+            reader = csv.DictReader(tsvin, delimiter='\t')
+            writer = csv.DictWriter(tsvout, delimiter='\t', fieldnames=self.outTsvFields)
+            writer.writeheader()
+            self.logger.info('output file at {}'.format(os.path.abspath(self.fin)))
+            count = 0
+            found = 0
+            for row in reader:
+                self.logger.info('reading row {:d}'.format(count))
+                id = row['trackid']
+                artist = row['artist']
+                title = row['title']
+                self.logger.info('processing track {} (artist: {}, title: {})'.format(id, artist, title))
+                for crawler in self.crawlers:
+                    lyrics = crawler.crawl(artist, title)
+                    if lyrics:
+                        self.logger.info('lyrics for {} retrieved by {}'.format(id, crawler))
+                        lyrics = self._inline(crawler.stripLyricsAuthor(lyrics))
+                        self.logger.debug('postprocessing - {}'.format(lyrics))
+                        writer.writerow({'trackid': id, 'lyrics': lyrics})
+                        self.logger.info('lyrics for {} written to output file'.format(id))
+                        found += 1
+                        self.logger.info('currently {} lyrics found'.format(found))
+                        break
+                    else:
+                        self.logger.warning('{} cannot retrieve lyrics'.format(crawler))
+                count += 1
+            self.logger.info('found {:d} lyrics out of {:d} tracks'.format(found, count))
+            self.logger.info('job completed')

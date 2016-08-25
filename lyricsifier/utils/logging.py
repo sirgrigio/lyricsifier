7af4eb34e7195cba6358d8eb02e85f901a41747c
Matteo Zaccagnino <matteozaccagnino@mail.com>
2016-08-25 12:30:14 +0200

    added utils modules

---
+++ lyricsifier/utils/logging.py
@@ -0,0 +1,16 @@
+import os
+import json
+import logging.config
+
+
+def loadCfg(default_path='logging.json',
+            default_level=logging.INFO,
+            env_key='LOG_CFG'):
+    env = os.getenv(env_key, None)
+    path = default_path if not env else env
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf8') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)

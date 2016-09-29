0345664b0e8dcf7bbe734c97b0af136fb283a7fe
Matteo Zaccagnino <matteozaccagnino@mail.com>
2016-09-29 19:24:36 +0200

    added job for clustering and classification

--- lyricsifier/core/job.py
+++ lyricsifier/core/job.py
@@ -2,10 +2,14 @@ import csv
 import logging
 import os
 import tempfile
+from sklearn.feature_selection import SelectKBest, chi2
+from lyricsifier.core.classification \
+    import Dataset, KMeansAlgorithm, PerceptronAlgorithm, \
+    MultinomialNBAlgorithm
 from lyricsifier.core.extractor \
     import MetroLyricsExtractor, LyricsComExtractor, \
     LyricsModeExtractor, AZLyricsExtractor
-from lyricsifier.core.tagger import LastFMTagger
+from lyricsifier.core.vectorizer import LyricsVectorizer
 from lyricsifier.core.worker import ExtractWorker, TagWorker
 from lyricsifier.core.utils import csv as csvutils, file
 
@@ -153,3 +157,92 @@ class TagJob:
                 worker.join()
             self._merge(workers)
         self.log.info('tag job completed')
+
+
+class ClusterJob():
+
+    def __init__(self, lyrics_file, tags_file, runs=10):
+        self.flyrics = lyrics_file
+        self.ftags = tags_file
+        self.runs = runs
+        self.log = logging.getLogger(__name__)
+
+    def _buildDataset(self):
+        data = []
+        labels = []
+        ftags_rows = csvutils.load(self.ftags)[0]
+        tags = {row['trackid']: row['tag'] for row in ftags_rows}
+        flyrics_rows = csvutils.load(self.flyrics)[0]
+        for row in flyrics_rows:
+            trackid = row['trackid']
+            lyrics = row['lyrics']
+            tag = tags.get(trackid, None)
+            if tag:
+                data.append(lyrics)
+                labels.append(tag)
+        return Dataset(data, labels)
+
+    def start(self):
+        self.log.info('setting up')
+        dataset = self._buildDataset()
+        kmc = KMeansAlgorithm(dataset, self.runs)
+        kmc.run()
+        self.log.info('clustering job completed')
+
+
+class ClassifyJob():
+
+    def __init__(self, lyrics_file, tags_file, outdir):
+        self.flyrics = lyrics_file
+        self.ftags = tags_file
+        self.outdir = outdir
+        self.vectorizer = LyricsVectorizer()
+        self.chisquared = SelectKBest(chi2)
+        self.log = logging.getLogger(__name__)
+
+    def _buildDataset(self):
+        data = []
+        labels = []
+        ftags_rows = csvutils.load(self.ftags)[0]
+        tags = {row['trackid']: row['tag'] for row in ftags_rows}
+        flyrics_rows = csvutils.load(self.flyrics)[0]
+        for row in flyrics_rows:
+            trackid = row['trackid']
+            lyrics = row['lyrics']
+            tag = tags.get(trackid, None)
+            if tag:
+                data.append(lyrics)
+                labels.append(tag)
+        return Dataset(data, labels)
+
+    def _setUp(self):
+        self.log.info('setting up')
+        file.mkdirs(self.outdir, safe=True)
+        dataset = self._buildDataset()
+        self.trainset, self.testset = Dataset.split(dataset, 0.8)
+        self.trainset.vectorize(self.vectorizer)
+        self.testset.vectorize(self.vectorizer, fit=False)
+
+    def start(self):
+        self._setUp()
+        features = self.trainset.data.shape[1]
+        for k in ['all', features // 2, features // 4, features // 8]:
+            trainset = Dataset(self.trainset.data, self.trainset.target)
+            testset = Dataset(self.testset.data, self.testset.target)
+            trainset.selectBestFeatures(self.chisquared, k)
+            testset.selectBestFeatures(self.chisquared, k, fit=False)
+            algorithms = [
+                PerceptronAlgorithm(trainset, testset),
+                MultinomialNBAlgorithm(trainset, testset)
+            ]
+            for alg in algorithms:
+                self.log.info(
+                    'running {:s} with {} features'.format(alg.name, k))
+                report = alg.run()
+                filename = '{:s}_{}.txt'.format(alg.name, k)
+                with open(
+                    os.path.join(self.outdir, filename),
+                    'w', encoding='utf8'
+                ) as fout:
+                    print(report, file=fout)
+        self.log.info('clustering job completed')
